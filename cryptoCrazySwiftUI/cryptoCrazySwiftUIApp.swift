957f1b6b9526153deae4c38b3f65b0aee025fef5
fatihfilizol <fatihfilizol@gmail.com>
2022-10-28 16:30:04 +0300

    Initial Commit

---
+++ cryptoCrazySwiftUI/cryptoCrazySwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  cryptoCrazySwiftUIApp.swift
+//  cryptoCrazySwiftUI
+//
+//  Created by Fatih Filizol on 28.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct cryptoCrazySwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
